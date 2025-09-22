5438a685808e2ce5c56a31a96c3a4101f511e0dc
Ravi Jain <raviajain@google.com>
2025-09-22 02:32:10 -0700

    Add timing checks and improve error messages for `perform` tests.
    The test now verifies that the actual vibration duration is at least the
    expected length returned by `perform`. Error messages are enhanced to
    include the specific effect and strength being tested.
    
    Bug: 441646240
    Test: atest VtsHalVibratorTargetTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I21c6e893eb7b0efb12487cd7db869a43cfa9b73f

--- vibrator/aidl/vts/VtsHalVibratorTargetTest.cpp
+++ vibrator/aidl/vts/VtsHalVibratorTargetTest.cpp
@@ -339,23 +339,29 @@ TEST_P(VibratorAidl, ValidateEffectWithCallback) {
         for (EffectStrength strength : kEffectStrengths) {
             auto callback = ndk::SharedRefBase::make<CompletionCallback>();
             int lengthMs = 0;
+            auto start = high_resolution_clock::now();
             ndk::ScopedAStatus status = vibrator->perform(effect, strength, callback, &lengthMs);
+            const std::string message =
+                    "\n  For effect: " + toString(effect) + " " + toString(strength);
 
             if (isEffectSupported) {
-                EXPECT_OK(std::move(status))
-                        << "\n  For effect: " << toString(effect) << " " << toString(strength);
-                EXPECT_GT(lengthMs, 0);
+                EXPECT_OK(std::move(status)) << message;
+                EXPECT_GT(lengthMs, 0) << message;
             } else {
-                EXPECT_UNKNOWN_OR_UNSUPPORTED(std::move(status))
-                        << "\n  For effect: " << toString(effect) << " " << toString(strength);
+                EXPECT_UNKNOWN_OR_UNSUPPORTED(std::move(status)) << message;
             }
 
             if (lengthMs <= 0) continue;
 
-            auto timeout = std::chrono::milliseconds(lengthMs) + VIBRATION_CALLBACK_TIMEOUT;
-            EXPECT_EQ(callback->wait_for(timeout), std::future_status::ready);
 
-            EXPECT_OK(vibrator->off());
+            auto expectedDuration = std::chrono::milliseconds(lengthMs);
+            auto timeout = expectedDuration + VIBRATION_CALLBACK_TIMEOUT;
+            EXPECT_EQ(callback->wait_for(timeout), std::future_status::ready) << message;
+            auto end = high_resolution_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+            EXPECT_GE(elapsed.count(), expectedDuration.count()) << message;
+            EXPECT_OK(vibrator->off()) << message;
         }
     }
 }
