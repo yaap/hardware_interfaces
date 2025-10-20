ee78a06a6240642835d24b7818b798387b78d307
Alice Wang <aliceywang@google.com>
2025-10-20 17:52:06 +0000

    see/devicestate: Add IDeviceState AIDL
    Adds a new AIDL interface, IDeviceState, to allow KeyMint
    in a VM to query the host's provisioning state.
    
    This is needed for KeyMint to determine if provisioning
    is allowed before proceeding with certain operations.
    
    Bug: 453056208
    Test: m
    
    Change-Id: I1b8475b095730921fd0d970140c41400a295508a

---
+++ security/see/devicestate/aidl/rules.mk
@@ -0,0 +1,34 @@
+
+# Copyright (C) 2025 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+AIDL_DIR := $(LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_AIDL_FLAGS :=
+
+MODULE_AIDL_LANGUAGE := rust
+
+MODULE_CRATE_NAME := android_hardware_security_see_devicestate
+
+MODULE_AIDL_PACKAGE := android/hardware/security/see/devicestate
+
+MODULE_AIDLS := \
+	$(AIDL_DIR)/$(MODULE_AIDL_PACKAGE)/IDeviceState.aidl \
+
+include make/aidl.mk
