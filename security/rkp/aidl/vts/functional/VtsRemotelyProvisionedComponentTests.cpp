67413b3a6898edff57e2f377434afaa141c55d80
Rajesh Nyamagoud <nyamagoud@google.com>
2025-10-31 17:44:55 +0000

    Guard NewKeyPerCallInTestMode against verification errors.
    Avoid crash when dereferencing BCC results that contain
    a backward-compatibility error.
    
    This amends CL: ag/36640850.
    
    Bug: 454238679
    Test: VtsHalRemotelyProvisionedComponentTargetTest
    Change-Id: I605dba7b3a340f663ad2369c58b3b6ad909010ed

--- security/rkp/aidl/vts/functional/VtsRemotelyProvisionedComponentTests.cpp
+++ security/rkp/aidl/vts/functional/VtsRemotelyProvisionedComponentTests.cpp
@@ -736,10 +736,13 @@ TEST_P(CertificateRequestTest, NewKeyPerCallInTestMode) {
     verifyCertificateRequestResult(secondBcc);
 
     // Verify that none of the keys in the first BCC are repeated in the second one.
-    for (const auto& i : *firstBcc) {
-        for (auto& j : *secondBcc) {
-            ASSERT_THAT(i.pubKey, testing::Not(testing::ElementsAreArray(j.pubKey)))
-                    << "Found a repeated pubkey in two generateCertificateRequest test mode calls";
+    if (firstBcc && secondBcc) {
+        for (const auto& i : *firstBcc) {
+            for (auto& j : *secondBcc) {
+                ASSERT_THAT(i.pubKey, testing::Not(testing::ElementsAreArray(j.pubKey)))
+                        << "Found a repeated pubkey in two generateCertificateRequest test mode "
+                           "calls";
+            }
         }
     }
 }
